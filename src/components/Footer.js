a20148894314002b16d95e249bd31b637180e732
arianexlee <arianelee718@gmail.com>
2022-12-31 18:18:49 -0500

    add to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,9 @@ import { SocialIcon } from 'react-social-icons';
 export function Footer(){
     return(
         <div className= "flex flex-col space-y-4 items-center px-[110px] py-[32px] text-lg md:flex-row md:justify-between">
-            <div className='font-sans text-center'>
-                Contact me at <span className='font-chivo underline'><a href="mailto:arialee@stanford.edu">arialee@stanford.edu</a></span>!
+            <div className='font-sans text-center md:text-start'>
+                <p className=''>Contact me at <span className='font-chivo underline'><a href="mailto:arialee@stanford.edu">arialee@stanford.edu</a></span>!</p>
+                <p className='text-sm italic'>This website was made with React</p>
             </div>
             <div className='space-x-4'>
                 <SocialIcon style={{width: '40px', height: '40px'}} bgColor='black' url="https://www.linkedin.com/in/ariane-lee-8b95ba192/"/>
